ad70106c0bc840eaff3193ecd53687378752e4dc
Andy Wu <awu200592@gmail.com>
2025-02-04 18:14:49 -0500

    everything is good

--- lab1/src/main.cpp
+++ lab1/src/main.cpp
@@ -1,17 +1,51 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+void MyDelay (unsigned long mSecondsApx);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    unsigned char portDDRC;
+    portDDRC = (unsigned char) 0x27;
+    portDDRC |= 0x80;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    unsigned charportC;
+    portC = (unsigned char *) 0x28;
+
+    *portC |= 0x80;
+    MyDelay(500);
+    *portC &= 0x7F;
+    MyDelay(500);
+
+    MyDelay(1000);
+
+    for (int i = 0; i < 2; i++) {
+        *portC |= 0x80;
+        MyDelay(500);
+        *portC &= 0x7F;
+        MyDelay(500);
+    }
+
+    MyDelay(1000);
+
+    for (int i = 0; i < 3; i++) {
+        *portC |= 0x80;
+        MyDelay(500);
+        portC &= 0x7F;
+        MyDelay(500);
+    }
+
+    MyDelay(1000);
 }
 
+void MyDelay (unsigned long mSecondsApx) {
+    volatile unsigned long i;
+    unsigned long endTime = 250 mSecondsApx;
+
+    for (i = 0; i < endTime; i++);
+}
+
+
+ 
 // put function definitions here:
 int myFunction(int x, int y) {
   return x + y;
